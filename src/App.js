50fa6fad7354bd137beabbb474e0d9d48b717f15
Arkadiusz Myszka <lokonhinio@gmail.com>
2024-02-18 22:32:34 +0100

    Fixed app.js adding .jsx to import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import { WelcomePage } from "./pages/WelcomePage/WelcomePage.jsx";
-import { RegisterPage } from "./pages/RegisterPage/RegisterPage";
+import { RegisterPage } from "./pages/RegisterPage/RegisterPage.jsx";
 
 function App() {
   return (
