5e4fcae884299a81cbfe4f6192d7ac8f0e87f5cb
Joanna Matwiej <126902190+Tworzaca0789@users.noreply.github.com>
2024-02-19 00:07:40 +0100

    Add recipe page and child components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { WelcomePage } from "./pages/WelcomePage/WelcomePage.jsx";
+import { RecipePage } from "./pages/RecipePage/RecipePage.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={ <WelcomePage />} />
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/recipe/:recipeId" element={<RecipePage />} />
       </Routes>
     </Router>
   );
