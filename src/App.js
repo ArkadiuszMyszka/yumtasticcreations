e2565069add8cd65b8bcd26e87ba84d198d74534
ewasawczynska <sawczynska.e@gmail.com>
2024-02-19 18:24:57 +0100

    Add SigninPage with LoginForm

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { WelcomePage } from "./pages/WelcomePage/WelcomePage.jsx";
 import { RegisterPage } from "./pages/RegisterPage/RegisterPage.jsx";
+import { SigninPage } from "./pages/SigninPage/SigninPage.jsx";
 import { RecipePage } from "./pages/RecipePage/RecipePage.jsx";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/signin" element={<SigninPage />} />
         <Route path="/recipe/:recipeId" element={<RecipePage />} />
       </Routes>
     </Router>
