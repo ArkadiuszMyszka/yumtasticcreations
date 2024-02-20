f9c5c5e502a405eeeb43548b623b2443e6785926
ewasawczynska <sawczynska.e@gmail.com>
2024-02-20 18:24:52 +0100

    Fix background in Register&Signin Page

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { AuthForm } from "../../components/AuthForm/AuthForm";
-import { Container, Link } from "./RegisterPage.styled";
+import { Container, Link, BgGraphics } from "./RegisterPage.styled";
 
 export const RegisterPage = () => {
     return (<Container> 
+        <BgGraphics/>
         <AuthForm/>
         <Link to="/signin">Sign In</Link>
     </Container>);
