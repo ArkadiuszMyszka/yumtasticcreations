cc1c1ecf0632f7f1a3247ab3c11070ccf9b5bccc
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-02-21 01:58:47 +0100

    Dużo poprawek
    Naprawa obsługi endpointów,
    Stworzenie komponentu PreviewCategories
    Stworzenie funkcji pobierających kategorie przepisów i funkcji pobierających przepisy na podstawie nazwy kategorii,
    renderowanie listy z kategoraiami i podlist z oknami przepisów pod każdą kategorie

--- src/components/PreviewCategories/PreviewCategories.jsx
+++ src/components/PreviewCategories/PreviewCategories.jsx
@@ -1,41 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectCategories } from "../../redux/categories/selectors";
 import { getMainPageRecipes } from "../../redux/recipes/operations";
 import { getCategories } from "../../redux/categories/operations";
-// import { selectRecipes, selectLoadingStatus, selectError } from "../../redux/recipes/selectors";
+import { useMediaQuery } from "@uidotdev/usehooks";
 
 export const PreviewCategories = () => {
     const dispatch = useDispatch();
     const categories = useSelector(selectCategories);
-    // const categoryRecipes = useSelector(selectRecipes);
-    // const isLoading = useSelector(selectLoadingStatus);
-    // const error = useSelector(selectError);
-    const beef = "Beef";
+    const [recipes, setRecipes] = useState([]);
     const popularCategories = categories.items;
-
+    const isMobile = useMediaQuery('(max-width:600px)');
+    const isTablet = useMediaQuery('(min-width:601px) and (max-width:1024px)');
+    
    useEffect(() => {
         dispatch(getCategories())
     }, [dispatch])
-
-
-    // useEffect(() => {
-    //     dispatch(getMainPageRecipes());
-    //   }, [dispatch]);
-    
-    // useEffect(() => {
-    //    const fetchRecipes = async () => {
-    //     try {
-    //         if (popularCategories.length > 0) {
-    //         popularCategories.forEach((category) => {
-    //         dispatch(getMainPageRecipes(category.title))
-    //         });
-    //      }
-    //     } catch (err) {
-    //         console.error("Wystąpił błąd:", err);
-    //     }
-    //    }
-    // }, [dispatch, popularCategories, recipes])
 
     useEffect(() => {
         const fetchRecipes = async () => {
@@ -45,32 +25,53 @@ export const PreviewCategories = () => {
                         return dispatch(getMainPageRecipes(category.title));
                     });
                     const results = await Promise.all(promises);
-                    return results;
-                }
+                    const resultsRecipes = results.map((category) => {
+                        return {
+                            title: category.meta.arg,
+                            recipes: category.payload,
+                    }
+                    })
+                    setRecipes([...resultsRecipes])}
             } catch (err) {
                 console.error("Wystąpił błąd:", err);
             }
         };
+        fetchRecipes();
+    }, [dispatch, popularCategories]);
 
-        fetchRecipes()
-            .then((results) => {
-                results.find(result => result.meta.arg === beef);
-            })
-            .catch((error) => {
-                console.error("Wystąpił błąd podczas pobierania przepisów:", error);
-            });
-    }, [dispatch, popularCategories, beef]);
+    useEffect(() => {
+        console.log("Updated recipes:", recipes);
+    }, [recipes]);
+
+    const getNumberOfRecipesToRender = () => {
+        if (isMobile) {
+            return 1;
+        } else if (isTablet) {
+            return 2;
+        } else {
+            return 4;
+        }
+    };
 
 return (
     <div>
         <ul>
-            {popularCategories.map((category) => (
-                <li key={category.id}>
+            {recipes.map((category) => (
+                <li>
                     <h1>{category.title}</h1>
                     <ul>
+                    {category.recipes.slice(0, getNumberOfRecipesToRender()).map((recipe) => {
+                            return ( 
+                            <li>    
+                            <h3>{recipe.title}</h3>
+                            <img src={recipe.thumb} alt={recipe.title} />
+                            </li>
+                            )
+                        })}
                     </ul>
                 </li>
             ))}
         </ul>
     </div>
 )
+};
