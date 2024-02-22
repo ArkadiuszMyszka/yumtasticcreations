8c11022b80917e1a22dcf7b32bc42dac2b74694c
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-02-22 22:21:40 +0100

    usuniecie proviedra z index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
-import store from './redux/store.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>  
   </React.StrictMode>
 );
 
