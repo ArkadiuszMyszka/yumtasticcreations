dd62b251726b3f192d744671f5eacef9c9823e27
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-02-22 22:13:08 +0100

    1

--- src/components/SpinachBackground/SpinachBackground.jsx
+++
@@ -1,8 +0,0 @@
-import { Spinach } from "./SpinachBackground.styled";
-import spinachIMG from "../../images/graphics/spinach_3.png"
-
-export const SpinachBackground = () => {
-    return (
-        <Spinach src={spinachIMG}/>
-    );
-};
--- src/components/SpinachBackground/SpinachBackground.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Spinach = styled.img`
-position: relative;
-bottom: -500px;
-z-index: -1;
-`
