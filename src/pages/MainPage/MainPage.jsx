d8ee1b295cba4058dee3488f94dd72e259238f10
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-02-24 04:39:30 +0100

    1

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { PreviewCategories } from "../../components/PreviewCategories/PreviewCategories";
-import Footer from "../../components/Footer/Footer";
+import {Footer} from "../../components/Footer/Footer";
 import { Main } from "./MainPage.styled"; 
 import { Yummy } from "../../components/Yummy/Yummy";
 import { Header } from "../../components/Header/Header"
