7b0dbff9dc415333eee7db150a5d861e09c79f4d
Arkadiusz Myszka <lokonhinio@gmail.com>
2024-02-25 08:33:50 +0100

    Authorization

--- src/services/PrivateApi.js
+++ src/services/PrivateApi.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const privateApi = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+  },
 });
 
 export default privateApi;
