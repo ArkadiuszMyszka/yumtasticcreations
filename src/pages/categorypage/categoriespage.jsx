43b3828e30c7a203b1406966417b40d209a101e3
GodOfOni <118010221+GodOfOni@users.noreply.github.com>
2024-02-25 20:19:32 +0100

    Update categoriespage.jsx

--- src/pages/categorypage/categoriespage.jsx
+++ src/pages/categorypage/categoriespage.jsx
@@ -1,7 +1,7 @@
 // CategoriesPage.jsx
-import CategoriesList from "../../components/categories/categorieslist/categorieslist"; // Adjusted path
-import SearchRecipesList from "../../components/categories/categorylist/categorylist"; // Adjusted path
-import { PageWrapper } from "../../components/categories/categorieslist/categorieslist.styled"; // Adjusted path
+import CategoriesList from "../../components/categorieslist/categorieslist"; // Adjusted path
+import SearchRecipesList from "../../components/categorylist/categorylist"; // Adjusted path
+import { PageWrapper } from "../../components/categorieslist/categorieslist.styled"; // Adjusted path
 import { Title } from "../../components/ui/mainpagetitle/mainpagetitle.styled"; // Adjusted path
 
 
