204e41dfd1a070874195d5e5cc5c63b71138212c
GodOfOni <118010221+GodOfOni@users.noreply.github.com>
2024-02-25 20:38:23 +0100

    Update categorylist.jsx

--- src/components/categorylist/categorylist.jsx
+++ src/components/categorylist/categorylist.jsx
@@ -6,7 +6,8 @@ import { getCategoryRecipes } from "redux/categories/operations";
 import { selectCategories } from "redux/categories/selectors";
 
 import Alert from "Components/ui/Alert";
-import RecipesList from "Components/images/ui/RecipesList/RecipesList";
+import RecipesList from "src/components/SearchPage/RecipesList
+/RecipesList";
 import CategorySkeleton from "Components/ui/Skeletons/CategorySkeleton";
 import CategoryPagination from "../Pagination/Pagination";
 
